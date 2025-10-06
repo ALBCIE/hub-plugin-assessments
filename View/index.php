8a106d7e7c17ddc58244e048ed208836987c4ced
Louis Ouellet <louis_ouellet@hotmail.com>
2025-10-06 14:13:02 -0400

    General: Initial Commit

---
+++ View/index.php
@@ -0,0 +1,20 @@
+<?php
+function gen($route, $nav){
+    if(empty($nav['link'])) return '';
+    if (strpos($route, '.') !== false) { $url = 'https://'.$nav['link']; } else { $url = $nav['link']; }
+    $html = '<a href="'.$url.'" class="col">
+                <div class="card">
+                    <i class="bi bi-'.$nav['icon'].' fs-1"></i>
+                    <span>'.$nav['label'].'</span>
+                </div>
+            </a>';
+    return $html;
+}
+?>
+<article class="assessments" id="layout">
+    <div class="row row-cols-1 row-cols-md-4 row-cols-lg-6">
+        <?php foreach($this->Builder->menu('assessments', '/assessments', 1) as $route => $nav): ?>
+            <?= gen($route, $nav) ?>
+        <?php endforeach; ?>
+    </div>
+</article>
